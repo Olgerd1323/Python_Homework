0d473e9d3c0987642c98b8478371a5ebb6294195
Oleg Makarenkov <azazel20hell@gmail.com>
2021-09-07 00:49:44 +0300

    python first homework

---
+++ HW_1.py
@@ -0,0 +1,40 @@
+p_1 = "Oleg"
+p_2 = 27
+p_3 = 85.5
+p_4 = bytes(9)
+p_5 = ['tender', 'contract', 'space marines']
+p_6 = tuple('attack')
+p_7 = set('hellooo buddyyy')
+p_8 = frozenset('hellooo buddyyy')
+p_9 = {'one': 1, 'two': 2, 'three': 3}
+print("p_1 =", p_1, type(p_1))
+print("p_2 =", p_2, type(p_2))
+print("p_3 =", p_3, type(p_3))
+print("p_4 =", p_4, type(p_4))
+print("p_5 =", p_5, type(p_5))
+print("p_6 =", p_6, type(p_6))
+print("p_7 =", p_7, type(p_7))
+print("p_8 =", p_8, type(p_8))
+print("p_9 =", p_9, type(p_9))
+
+first_name = 'Oleg'
+last_name = 'Makarenkov'
+full_name = first_name + last_name
+print('full_name =', full_name, type(full_name))
+
+a = 9
+b = 4
+c = a + b
+print('c =', c, type(c))
+
+d = a / b
+print('d =', d, type(d))
+
+e = a * b
+print('e =', e, type(e))
+
+f = a // b
+print('f =', f, type(f))
+
+g = a % b
+print('g =', g, type(g))
